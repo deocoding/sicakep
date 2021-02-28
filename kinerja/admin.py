0f2454cf3efdc79c09fa8f65d015e2ad8a5050b7
deocoding PC <deocoding@gmail.com>
2021-02-28 23:36:52 +0700

    dari: Laptop

--- kinerja/admin.py
+++ kinerja/admin.py
@@ -1,10 +1,50 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Jabatan)
-admin.site.register(Pegawai)
-admin.site.register(PejabatPenilai)
-admin.site.register(Sasaran)
-admin.site.register(Satuan)
-admin.site.register(Indikator)
+# admin.site.register(Jabatan)
+@admin.register(Jabatan)
+class JabatanAdmin(admin.ModelAdmin):
+    list_display = ("nama", )
+    search_fields = ["nama",]
+
+    # def get_form(self, request, obj=None, **kwargs):
+    #     form = super().get_form(request, obj, **kwargs)
+    #     form.base_fields["nama"].label = "Jabatan"
+    #     return form
+
+# admin.site.register(Pegawai)
+@admin.register(Pegawai)
+class PegawaiAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['user', 'jabatan']
+    list_display = ("nama", "nip", "jabatan")
+    search_fields = (["nama", "nip", "jabatan__nama"])
+
+# admin.site.register(PejabatPenilai)
+@admin.register(PejabatPenilai)
+class PejabatPenilaiAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['atasan', 'pegawai']
+    list_display = ("atasan", "pegawai")    
+    search_fields = (["atasan__nama", "pegawai__nama"])
+
+# admin.site.register(Sasaran)
+@admin.register(Sasaran)
+class SasaranAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['pegawai']
+    list_display = ("nama", "pegawai")    
+    search_fields = (["nama", "pegawai__nama"])
+
+# admin.site.register(Satuan)
+@admin.register(Satuan)
+class SatuanAdmin(admin.ModelAdmin):
+    list_display = ("nama", )
+    search_fields = ["nama",]
+
+# admin.site.register(Indikator)
+@admin.register(Indikator)
+class IndikatorAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['sasaran', 'satuan']
+    list_display = ("sasaran", "nama")    
+    search_fields = (["sasaran__nama", "nama", ])
+
+admin.site.register(BulanPenyelesaian)
 admin.site.register(Realisasi)
--- kinerja/models.py
+++ kinerja/models.py
@@ -1,23 +1,25 @@
+from typing import ClassVar
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 
 
 class Jabatan(models.Model):
-    nama = models.TextField(max_length=100,  null=True)
+    nama = models.TextField(verbose_name='Nama Jabatan', max_length=100, null=True, unique=True)
 
     def __str__(self):
         return self.nama
 
     class Meta:
-        verbose_name_plural = '      Jabatan'
+        verbose_name_plural = '       Jabatan'
 
 
 class Pegawai(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    nip = models.CharField(max_length=18, null=True)
-    nama = models.CharField(max_length=100, null=True)
+    nama = models.CharField(verbose_name='Nama Pegawai', max_length=100, null=True)
+    nip = models.CharField(verbose_name='NIP', max_length=21, null=True)
     pangkat = models.CharField(max_length=100, null=True)
-    golrung = models.CharField(max_length=5, null=True)
+    golrung = models.CharField(verbose_name='Golongan(Ruang)', max_length=5, null=True)
     jabatan = models.ForeignKey(Jabatan, null=True, blank=True, on_delete=models.SET_NULL)
     # jabnil = 
 
@@ -25,30 +27,30 @@ class Pegawai(models.Model):
         return self.nama
 
     class Meta:
-        verbose_name_plural = '     Pegawai'
+        verbose_name_plural = '      Pegawai'
 
 
 class PejabatPenilai(models.Model):
-    pegawai = models.ForeignKey(Pegawai, null=True, on_delete=models.SET_NULL, related_name='pegawai')
-    atasan = models.ForeignKey(Pegawai, null=True, on_delete=models.SET_NULL, related_name='atasan')
+    atasan = models.ForeignKey(Pegawai, null=True, on_delete=models.SET_NULL, related_name='atasan', verbose_name='Pejabat Penilai')
+    pegawai = models.ForeignKey(Pegawai, null=True, on_delete=models.SET_NULL, related_name='pegawai', verbose_name='Pegawai Yang Dinilai')
 
     def __str__(self):
         return self.atasan.nama
 
     class Meta:
-        verbose_name_plural = '    Pejabat Penilai'
+        verbose_name_plural = '     Pejabat Penilai'
 
 
 class Sasaran(models.Model):
-    pegawai = models.ForeignKey(Pegawai, null=True, on_delete=models.CASCADE)
-    nama = models.TextField('Sasaran Kegiatan', max_length=500, null=True)
+    nama = models.TextField(verbose_name='Sasaran Kegiatan', max_length=500, null=True)
     tahun = models.CharField(max_length=4, null=True)
+    pegawai = models.ForeignKey(Pegawai, null=True, on_delete=models.CASCADE, verbose_name='Nama Pegawai')
 
     def __str__(self):
-        return '%s - %s' % (self.pegawai.nama, self.nama)
+        return self.nama
 
     class Meta:
-        verbose_name_plural = '   Sasaran Kegiatan'
+        verbose_name_plural = '    Sasaran Kegiatan'
 
 
 class Satuan(models.Model):
@@ -58,22 +60,45 @@ class Satuan(models.Model):
         return self.nama
 
     class Meta:
-        verbose_name_plural = '  Satuan'
+        verbose_name_plural = '   Satuan'
 
-
-class Indikator(models.Model):
-    sasaran = models.ForeignKey(Sasaran, null=True, on_delete=models.CASCADE)
-    nama = models.TextField(max_length=500, null=True)
-    angka_kredit = models.IntegerField(null=True, default=0)
-    kuantitas = models.IntegerField(null=True, default=1)
-    satuan = models.OneToOneField(Satuan, null=True, on_delete=models.SET_NULL)
-    kualitas = models.IntegerField(null=True, default=100)
+class BulanPenyelesaian(models.Model):
+    nama = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.nama
 
     class Meta:
-        verbose_name_plural = ' Indikator Kegiatan'
+        verbose_name_plural = '  Bulan'
+
+class Indikator(models.Model):
+    BULAN = (
+        ('Januari', 'Januari'),
+        ('Februari', 'Februari'),
+        ('Maret', 'Maret'),
+        ('April', 'April'),
+        ('Mei', 'Mei'),
+        ('Juni', 'Juni'),
+        ('Juli', 'Juli'),
+        ('Agustus', 'Agustus'),
+        ('September', 'September'),
+        ('Oktober', 'Oktober'),
+        ('November', 'November'),
+        ('Desember', 'Desember'),
+    )
+    sasaran = models.ForeignKey(Sasaran, null=True, on_delete=models.CASCADE, verbose_name='Sasaran Kegiatan')
+    nama = models.TextField('Indikator Kinerja', max_length=500, null=True)
+    target_mutu = models.IntegerField(null=True, default=100, verbose_name='Target Mutu')
+    target_kuantitas = models.IntegerField(null=True, default=1)
+    satuan = models.ForeignKey(Satuan, null=True, on_delete=models.SET_NULL)
+    waktu_penyelesaian = models.ManyToManyField(BulanPenyelesaian)
+    pagu_anggaran = models.DecimalField(decimal_places=2, max_digits=15, null=True, blank=True)
+
+    def __str__(self):
+        return self.nama
+
+    class Meta:
+        verbose_name_plural = ' Indikator Kinerja'
 
 
 class Realisasi(models.Model):
